e5b19536a4a2d1eef489db4644b69ff725cffeb2
Daniel Kazemian <dkazemian@gmail.com>
2016-06-01 16:00:02 -0400

    Update problem3.js

--- problem3.js
+++ problem3.js
@@ -14,3 +14,24 @@ while(a<prime){
 }
 return prime
 }
+/*
+Ruby Version
+def euler3(number)
+    a=2
+    primefactors=[]
+    while a<number do
+        if number%a==0 then
+            number=(number/a)
+            primefactors.push(a)
+            a=2
+        else
+            a+=1
+        end
+        
+    end
+    
+    primefactors.push(number)
+    return primefactors
+end
+euler3(600851475143)
+*/
