31db7f437acd3b85b9f1ef6780016825545dc27d
Daniel Kazemian <dkazemian@gmail.com>
2016-06-01 15:41:09 -0400

    Update problem2.js

--- problem2.js
+++ problem2.js
@@ -15,3 +15,24 @@ while(a<number){
 }
 return sum;
 }
+/*
+Ruby Version
+def euler2(number)
+    a=0
+    b=1
+    c=0
+    sum=0
+    numbers=[]
+    while a<number do
+        if a%2==0 
+            sum+=a
+        end
+        c=a+b
+        a=b
+        b=c
+        numbers.push(c)
+    end
+    return sum
+end
+euler2(4000000) 
+*/
